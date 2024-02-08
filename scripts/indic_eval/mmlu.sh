9354eedcef64d288a5e2c10fa2ea5565b68d2bbe
Manish Prakash <manish.prakash@citymall.live>
2024-02-08 18:06:15 +0530

    updating scripts to make it easier to run

--- scripts/indic_eval/mmlu.sh
+++ scripts/indic_eval/mmlu.sh
@@ -60,23 +60,23 @@ FOLDER_BASE=/sky-notebook/eval-results
 for model_name_or_path in "${model_names[@]}"; do
     model_name=${model_name_or_path##*/}
     TASK_NAME=mmlu_hi_translated
-    NUM_SHOTS=0short
+    # NUM_SHOTS=0short
     
-    FOLDER="${FOLDER_BASE}/${TASK_NAME}/${model_name}/${NUM_SHOTS}"
-    FILE=$FOLDER/metrics.json
+    # FOLDER="${FOLDER_BASE}/${TASK_NAME}/${model_name}/${NUM_SHOTS}"
+    # FILE=$FOLDER/metrics.json
 
-    if [ ! -f "$FILE" ]; then
-        # zero-shot
-        python3 -m eval.mmlu.run_eval \
-            --ntrain 0 \
-            --data_dir data/eval/mmlu_hi_translated \
-            --save_dir $FOLDER \
-            --model_name_or_path $model_name_or_path \
-            --tokenizer_name_or_path $model_name_or_path \
-            --eval_batch_size 4 \
-            --use_chat_format \
-            --chat_formatting_function eval.templates.create_prompt_with_chatml_format
-    fi
+    # if [ ! -f "$FILE" ]; then
+    #     # zero-shot
+    #     python3 -m eval.mmlu.run_eval \
+    #         --ntrain 0 \
+    #         --data_dir data/eval/mmlu_hi_translated \
+    #         --save_dir $FOLDER \
+    #         --model_name_or_path $model_name_or_path \
+    #         --tokenizer_name_or_path $model_name_or_path \
+    #         --eval_batch_size 4 \
+    #         --use_chat_format \
+    #         --chat_formatting_function eval.templates.create_prompt_with_chatml_format
+    # fi
 
     NUM_SHOTS=5short
     FOLDER="${FOLDER_BASE}/${TASK_NAME}/${model_name_or_path}/${NUM_SHOTS}"
