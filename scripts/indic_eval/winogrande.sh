fd35ec9a1504508d3e4be4d159537dabe24fc47d
Manish Prakash <manish.prakash@citymall.live>
2024-02-08 09:36:53 +0530

    updating scripts to make it easier to run

--- scripts/indic_eval/winogrande.sh
+++ scripts/indic_eval/winogrande.sh
@@ -1,16 +1,27 @@
 export CUDA_VISIBLE_DEVICES=0
 
 
-model_name_or_path="manishiitg/open-aditi-hi-v2"
+model_names=(
+    "manishiitg/open-aditi-hi-v2"
+    "manishiitg/open-aditi-hi-v1"
+)
+FOLDER_BASE=/sky-notebook/eval-results
 
-echo "evaluating open-aditi-v2 base on winogrande ..."
 
-# zero-shot
-python3 -m eval.winogrande.run_eval \
-    --ntrain 0 \
-    --save_dir $FOLDER \
-    --model_name_or_path $model_name_or_path \
-    --tokenizer_name_or_path $model_name_or_path \
-    --eval_batch_size 8 \
+for model_name_or_path in "${model_names[@]}"; do
+    model_name=${model_name_or_path##*/}
+    TASK_NAME=winogrande
+    NUM_SHOTS=0short
+    
+    FOLDER="${FOLDER_BASE}/${TASK_NAME}/${model_name}/${NUM_SHOTS}"
+    FILE=$FOLDER/metrics.json
+
+    if [ ! -f "$FILE" ]; then
+        python3 -m eval.winogrande.run_eval \
+            --ntrain 0 \
+            --save_dir $FOLDER \
+            --model_name_or_path $model_name_or_path \
+            --tokenizer_name_or_path $model_name_or_path \
+            --eval_batch_size 8 \
             --use_chat_format \
             --chat_formatting_function eval.templates.create_prompt_with_chatml_format
--- scripts/indic_eval/xlsum.sh
+++ scripts/indic_eval/xlsum.sh
@@ -1,17 +1,29 @@
 export CUDA_VISIBLE_DEVICES=0
 
 
-model_name_or_path="manishiitg/open-aditi-hi-v2"
+model_names=(
+    "manishiitg/open-aditi-hi-v2"
+    "manishiitg/open-aditi-hi-v1"
+)
+FOLDER_BASE=/sky-notebook/eval-results
 
-echo "evaluating open-aditi-v2 base on xlsum ..."
 
-# 1-shot
-python3 -m eval.xlsum.run_eval \
-    --ntrain 1 \
-    --max_context_length 512 \
-    --save_dir $FOLDER \
-    --model_name_or_path $model_name_or_path \
-    --tokenizer_name_or_path $model_name_or_path \
-    --eval_batch_size 1 \
+for model_name_or_path in "${model_names[@]}"; do
+    model_name=${model_name_or_path##*/}
+    TASK_NAME=xlsum
+    NUM_SHOTS=1short
+    
+    FOLDER="${FOLDER_BASE}/${TASK_NAME}/${model_name}/${NUM_SHOTS}"
+    FILE=$FOLDER/metrics.json
+
+    if [ ! -f "$FILE" ]; then
+        # 1-shot
+        python3 -m eval.xlsum.run_eval \
+            --ntrain 1 \
+            --max_context_length 512 \
+            --save_dir $FOLDER \
+            --model_name_or_path $model_name_or_path \
+            --tokenizer_name_or_path $model_name_or_path \
+            --eval_batch_size 1 \
             --use_chat_format \
             --chat_formatting_function eval.templates.create_prompt_with_chatml_format
