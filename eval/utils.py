28d4835e39b606cda04818a7f5c4cc1c3f63c372
Manish Prakash <manish.prakash@citymall.live>
2024-02-09 17:01:23 +0530

    updates

--- eval/utils.py
+++ eval/utils.py
@@ -163,8 +163,8 @@ def get_next_word_predictions(
         ]
         
         batch_probs = torch.softmax(batch_logits, dim=-1)
-        # if candidate_token_ids is not None:
-        #     batch_probs = batch_probs[:, candidate_token_ids]
+        if candidate_token_ids is not None:
+            batch_probs = batch_probs[:, candidate_token_ids]
         print("batch_probs", batch_probs)
 
         batch_prediction_indices = torch.argmax(batch_probs, dim=-1)
