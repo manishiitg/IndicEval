6c0b941baf4097b2826424939729c6720bd0b0d2
Manish Prakash <manish.prakash@citymall.live>
2024-02-09 22:20:30 +0530

    updates

--- scripts/indic_eval/arc.sh
+++ scripts/indic_eval/arc.sh
@@ -33,7 +33,9 @@ for model_name_or_path in "${model_names[@]}"; do
             --tokenizer_name_or_path $model_name_or_path \
             --eval_batch_size 4 \
             --use_chat_format \
-            --chat_formatting_function eval.templates.create_prompt_with_chatml_format
+            --chat_formatting_function eval.templates.create_prompt_with_chatml_format \
+            --use_vllm \
+            --awq
     fi
 
     # NUM_SHOTS=5short
