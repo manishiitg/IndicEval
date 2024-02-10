46d1263dbd05030822630450fb3a59fcf8980a65
Manish Prakash <manish.prakash@citymall.live>
2024-02-10 08:21:00 +0530

    fixes

--- scripts/indic_eval/boolq.sh
+++ scripts/indic_eval/boolq.sh
@@ -28,7 +28,7 @@ for model_name_or_path in "${model_names[@]}"; do
             --save_dir $FOLDER \
             --model_name_or_path $model_name_or_path \
             --tokenizer_name_or_path $model_name_or_path \
-            --eval_batch_size 6 \
+            --eval_batch_size 4 \
             --use_chat_format \
             --chat_formatting_function eval.templates.create_prompt_with_chatml_format \
             --use_vllm \
@@ -57,7 +57,7 @@ for model_name_or_path in "${model_names[@]}"; do
             --save_dir $FOLDER \
             --model_name_or_path $model_name_or_path \
             --tokenizer_name_or_path $model_name_or_path \
-            --eval_batch_size 6 \
+            --eval_batch_size 4 \
             --use_chat_format \
             --chat_formatting_function eval.templates.create_prompt_with_chatml_format \
             --awq
@@ -85,7 +85,7 @@ for model_name_or_path in "${model_names[@]}"; do
             --save_dir $FOLDER \
             --model_name_or_path $model_name_or_path \
             --tokenizer_name_or_path $model_name_or_path \
-            --eval_batch_size 6 \
+            --eval_batch_size 4 \
             --use_chat_format \
             --chat_formatting_function eval.templates.create_prompt_with_chatml_format \
             --use_vllm \
@@ -110,7 +110,7 @@ for model_name_or_path in "${model_names[@]}"; do
             --save_dir $FOLDER \
             --model_name_or_path $model_name_or_path \
             --tokenizer_name_or_path $model_name_or_path \
-            --eval_batch_size 6 \
+            --eval_batch_size 4 \
             --use_chat_format \
             --chat_formatting_function eval.templates.create_prompt_with_chatml_format \
             --awq
