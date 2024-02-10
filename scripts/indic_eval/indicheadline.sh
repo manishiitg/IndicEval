ffe30b211a538acf0bdbd59dd072712848d0faa6
Manish Prakash <manish.prakash@citymall.live>
2024-02-10 18:19:10 +0530

    fixes

--- scripts/indic_eval/indicheadline.sh
+++ scripts/indic_eval/indicheadline.sh
@@ -20,7 +20,7 @@ for model_name_or_path in "${model_names[@]}"; do
     echo "evaluating $model_name base on $TASK_NAME $NUM_SHOTS ..."
     if [ ! -f "$FILE" ]; then
         # 1-shot
-        python3 -m eval.indicheadline.run_eval_exact \
+        python3 -m eval.indicheadline.run_translate_test_eval \
         --ntrain 0 \
         --max_context_length 3750 \
         --save_dir $FOLDER \
