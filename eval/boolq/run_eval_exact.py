2e4c06b14beae62f4ee0715caf72392f7f530463
Manish Prakash <manish.prakash@citymall.live>
2024-02-10 08:37:02 +0530

    fixes

--- eval/boolq/run_eval_exact.py
+++ eval/boolq/run_eval_exact.py
@@ -75,8 +75,8 @@ def eval_hf_model(args, model, tokenizer, prompts, test_data, batch_size=1):
     predictions = []
     idx = 0
     for row in test_data:
-        # outputs[idx] = outputs[idx].split('.')[0]
-        # targets[idx] = targets[idx].split('.')[0]
+        outputs[idx] = outputs[idx].split('.')[0]
+        targets[idx] = targets[idx].split('.')[0]
         row = {
             "question": row["question"],
             "model_output": outputs[idx],
