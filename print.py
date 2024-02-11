8f0ae9804d264fe2f267a957eea65ed6d19ce293
Manish Prakash <manish.prakash@citymall.live>
2024-02-11 20:15:40 +0530

    fixes

--- print.py
+++ print.py
@@ -11,13 +11,14 @@ for root, dirs, files in os.walk(directory):
             file_path = os.path.join(root, file)
             
             print(file_path)
-            splits = file_path.replace(directory, "").split('/')
-            task = splits[0]
-            sub_task = splits[1]
-            model = splits[2]
-            shot = splits[3]
-            file = splits[4]
             if file == "metrics.json":
+                splits = file_path.replace(directory, "").split('/')
+                task = splits[0]
+                sub_task = splits[1]
+                model = splits[2]
+                shot = splits[3]
+                file = splits[4]
+                
                 with open(file_path, 'r') as json_file:
                     try:
                         metric = json.load(json_file)
