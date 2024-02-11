26183aecb99f8b11b8846ab811d02296e619ad21
Manish Prakash <manish.prakash@citymall.live>
2024-02-11 12:27:00 +0530

    fixes

--- print.py
+++ print.py
@@ -1,18 +1,25 @@
 import os
 import json
 
-directory = "/sky-notebook/eval-results"
+directory = "/sky-notebook/eval-results/"
 
 for root, dirs, files in os.walk(directory):
     for file in files:
         if file.endswith('.json'):
             file_path = os.path.join(root, file)
             print(file_path)
-            # with open(file_path, 'r') as json_file:
-            #     try:
-            #         data = json.load(json_file)
-            #         print(f"Contents of {file}:")
-            #         print(json.dumps(data, indent=4))
-            #     except json.JSONDecodeError as e:
-            #         print(f"Error decoding JSON in {file}: {e}")
 
+            splits = file_path.replace(directory, "").split(['/'])
+            task = splits[0]
+            sub_task = splits[1]
+            model = splits[2]
+            shot = splits[3]
+            file = splits[4]
+            if file == "metrics.json":
+                with open(file_path, 'r') as json_file:
+                    try:
+                        data = json.load(json_file)
+                        print(f"Contents of {file}:")
+                        print(json.dumps(data, indent=4))
+                    except json.JSONDecodeError as e:
+                        print(f"Error decoding JSON in {file}: {e}")
