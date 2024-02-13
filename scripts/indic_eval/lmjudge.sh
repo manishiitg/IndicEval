955fb11eed7efbd0163abbb5f40cf9c36d772b06
Manish Prakash <manish.prakash@citymall.live>
2024-02-13 14:19:26 +0530

    fixes

--- scripts/indic_eval/lmjudge.sh
+++ scripts/indic_eval/lmjudge.sh
@@ -3,10 +3,11 @@ export CUDA_VISIBLE_DEVICES=0
 
 model_names=(
     # "LoneStriker/Smaug-72B-v0.1-AWQ"
-    "Qwen/Qwen1.5-72B-Chat-AWQ"
+    # "Qwen/Qwen1.5-72B-Chat-AWQ"
     "manishiitg/open-aditi-hi-v2-awq"
     "manishiitg/open-aditi-hi-v1-awq"
     "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ"
+    "manishiitg/open-aditi-hi-v2-dpo-awq-1.1"
     "manishiitg/open-aditi-hi-v2"
 )
 FOLDER_BASE=/sky-notebook/eval-results/lmjudge
