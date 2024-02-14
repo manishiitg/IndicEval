b3e33ee887ee74f7f26ed6d903f33db44c2962b5
Manish Prakash <manish.prakash@citymall.live>
2024-02-14 17:23:55 +0530

    updates

--- scripts/indic_eval/common_vars.sh
+++ scripts/indic_eval/common_vars.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 model_names=(
-    "Qwen/Qwen1.5-72B-Chat-AWQ"
+    # "Qwen/Qwen1.5-72B-Chat-AWQ"
     "manishiitg/open-aditi-hi-v2-awq"
-    "manishiitg/open-aditi-hi-v1-awq"
-    "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ"
-    "manishiitg/open-aditi-hi-v2-dpo-awq-1.1"
-    "manishiitg/open-aditi-hi-v2"
+    # "manishiitg/open-aditi-hi-v1-awq"
+    # "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ"
+    # "manishiitg/open-aditi-hi-v2-dpo-awq-1.1"
+    # "manishiitg/open-aditi-hi-v2"
 )
--- scripts/indic_eval/xlsum.sh
+++ scripts/indic_eval/xlsum.sh
@@ -7,19 +7,26 @@ FOLDER_BASE=/sky-notebook/eval-results
 for model_name_or_path in "${model_names[@]}"; do
     model_name=${model_name_or_path##*/}
     TASK_NAME=xlsum
-    NUM_SHOTS=1short
+    NUM_SHOTS=0short
     
     FOLDER="${FOLDER_BASE}/${TASK_NAME}/${model_name}/${NUM_SHOTS}"
     FILE=$FOLDER/metrics.json
+    echo "evaluating $model_name base on $TASK_NAME $NUM_SHOTS ..."
+
+    if echo "$model_name" | grep -qi "awq"; then
+        awq_param="--awq"
+    else
+        awq_param=""
 
     if [ ! -f "$FILE" ]; then
         # 1-shot
         python3 -m eval.xlsum.run_eval \
             --ntrain 1 \
-            --max_context_length 512 \
+            --max_context_length 3750 \
             --save_dir $FOLDER \
             --model_name_or_path $model_name_or_path \
             --tokenizer_name_or_path $model_name_or_path \
             --eval_batch_size 1 \
             --use_chat_format \
-            --chat_formatting_function eval.templates.create_prompt_with_chatml_format
+            --chat_formatting_function eval.templates.create_prompt_with_chatml_format \
+            $awq_param
