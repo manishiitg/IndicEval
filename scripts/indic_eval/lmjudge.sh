5bf57bda053b71cfc83c32efa92216c30986bda5
Manish Prakash <manish.prakash@citymall.live>
2024-02-14 12:35:23 +0530

    updates

--- scripts/indic_eval/lmjudge.sh
+++ scripts/indic_eval/lmjudge.sh
@@ -1,6 +1,6 @@
 model_names=(
-    "LoneStriker/Smaug-72B-v0.1-AWQ"
-    # "Qwen/Qwen1.5-72B-Chat-AWQ"
+    # "LoneStriker/Smaug-72B-v0.1-AWQ"
+    "Qwen/Qwen1.5-72B-Chat-AWQ"
     "manishiitg/open-aditi-hi-v2-awq"
     "manishiitg/open-aditi-hi-v1-awq"
     "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ"
