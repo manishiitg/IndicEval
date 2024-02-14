701d480e6f0fd4ee595d51fd697caf0273a9ad68
Manish Prakash <manish.prakash@citymall.live>
2024-02-14 17:49:18 +0530

    updates

--- scripts/indic_eval/xlsum.sh
+++ scripts/indic_eval/xlsum.sh
@@ -6,7 +6,7 @@ FOLDER_BASE=/sky-notebook/eval-results
 
 for model_name_or_path in "${model_names[@]}"; do
     model_name=${model_name_or_path##*/}
-    TASK_NAME=xlsum
+    TASK_NAME=xlsum-hi
     NUM_SHOTS=0short
     
     FOLDER="${FOLDER_BASE}/${TASK_NAME}/${model_name}/${NUM_SHOTS}"
@@ -21,6 +21,36 @@ for model_name_or_path in "${model_names[@]}"; do
 
     if [ ! -f "$FILE" ]; then
         python3 -m eval.xlsum.run_hi_eval \
+            --lang hindi \
+            --max_context_length 3750 \
+            --save_dir $FOLDER \
+            --model_name_or_path $model_name_or_path \
+            --tokenizer_name_or_path $model_name_or_path \
+            --use_chat_format \
+            --chat_formatting_function eval.templates.create_prompt_with_chatml_format \
+            $awq_param
+    else
+        cat "$FILE"
+    fi
+done
+for model_name_or_path in "${model_names[@]}"; do
+    model_name=${model_name_or_path##*/}
+    TASK_NAME=xlsum
+    NUM_SHOTS=0short
+    
+    FOLDER="${FOLDER_BASE}/${TASK_NAME}/${model_name}/${NUM_SHOTS}"
+    FILE=$FOLDER/metrics.json
+    echo "evaluating $model_name base on $TASK_NAME $NUM_SHOTS ..."
+
+    if echo "$model_name" | grep -qi "awq"; then
+        awq_param="--awq"
+    else
+        awq_param=""
+    fi
+
+    if [ ! -f "$FILE" ]; then
+        python3 -m eval.xlsum.run_en_eval \
+            --lang english \
             --max_context_length 3750 \
             --save_dir $FOLDER \
             --model_name_or_path $model_name_or_path \
