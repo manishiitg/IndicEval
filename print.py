a4482140967b0a400dbe2c88b900b86e39bda6ea
Manish Prakash <manish.prakash@citymall.live>
2024-02-15 16:49:12 +0530

    updates

--- print.py
+++ print.py
@@ -53,7 +53,7 @@ def sort_data(data):
                     if lang not in sorted_data:
                         sorted_data[lang] = []
                     sorted_data[lang].append((task, model, metric, metric_value))
-                    break
+                    # break
                 
     for lang, data in sorted_data.items():
         # Sort the list based on the metric
@@ -74,3 +74,56 @@ def sort_data(data):
     return ret_data
 
 print(json.dumps(sort_data(scores), indent=4))
+
+def generate_markdown_table(data, task_names):
+    markdown_output = ""
+
+    # Iterate over tasks and sub-tasks
+    for lang, lang_dict in data.items():
+        markdown_output += f"## Language {lang.capitalize()}\n\n"
+
+        tasks = []
+        models = []
+        task_model_score = {}
+        for task, tasks_dict in lang_dict.items():
+            tasks.append(task)
+            if task not in task_model_score:
+                task_model_score[task] = {}
+            for model, model_dict in tasks_dict.items():
+                models.append(model)
+                if model not in task_model_score[task]:
+                    task_model_score[task][model] = {}
+                for metric, metric_value in model_dict.items():
+                    task_model_score[task][model] = metric_value
+                    break
+        
+        # Create a table header
+        
+        taskStr = "| "
+        dashStr = "| "
+        for task in tasks:
+            taskStr += task + " | "
+            dashStr += "--- | "
+
+        markdown_output += f"| Model {taskStr}\n"
+        markdown_output += f"| --- {dashStr}\n"
+
+        for model in models:
+            markdown_output += f"| {model} |"
+            for task in tasks:
+                average_value = task_model_score[task][model]
+                markdown_output += f" {average_value:.4f} |"
+
+            markdown_output += "\n"
+
+        # Add a newline after the table
+        markdown_output += "\n"
+
+    return markdown_output
+
+
+# Convert JSON to Markdown table grouped by task and sub-task
+markdown_output = generate_markdown_table(sort_data(scores))
+
+# Print the Markdown output
+print(markdown_output)
