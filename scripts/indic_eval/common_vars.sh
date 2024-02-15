52554b4b72809f16fbd8699fab55670345e4372b
Manish Prakash <manish.prakash@citymall.live>
2024-02-15 17:21:26 +0530

    updates

--- scripts/indic_eval/common_vars.sh
+++ scripts/indic_eval/common_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 model_names=(
-    "Qwen/Qwen1.5-72B-Chat-AWQ"
+    # "Qwen/Qwen1.5-72B-Chat-AWQ"
     "manishiitg/open-aditi-hi-v2-awq"
     "manishiitg/open-aditi-hi-v1-awq"
     "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ"
