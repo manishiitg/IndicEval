7f4817364327096b70b763d8717fc0aac0f948a8
Manish Prakash <manish.prakash@citymall.live>
2024-02-15 17:19:24 +0530

    updates

--- scripts/indic_eval/common_vars.sh
+++ scripts/indic_eval/common_vars.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 model_names=(
-    # "Qwen/Qwen1.5-72B-Chat-AWQ"
+    "Qwen/Qwen1.5-72B-Chat-AWQ"
     "manishiitg/open-aditi-hi-v2-awq"
     "manishiitg/open-aditi-hi-v1-awq"
     "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ"
-    # "teknium/OpenHermes-2.5-Mistral-7B"
-    # "manishiitg/open-aditi-hi-v2",
-    # "manishiitg/open-aditi-hi-v1"
+    "teknium/OpenHermes-2.5-Mistral-7B"
+    "manishiitg/open-aditi-hi-v2",
+    "manishiitg/open-aditi-hi-v1"
     "ai4bharat/Airavata"
 )
 FOLDER_BASE=/sky-notebook/eval-results
