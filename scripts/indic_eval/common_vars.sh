86945005571a21165233d708c4e4829e74369dd9
Manish Prakash <manish.prakash@citymall.live>
2024-02-20 12:57:10 +0530

    updates

--- scripts/indic_eval/common_vars.sh
+++ scripts/indic_eval/common_vars.sh
@@ -2,12 +2,12 @@
 
 model_names=(
     # "Qwen/Qwen1.5-72B-Chat-AWQ"
-    "manishiitg/open-aditi-hi-v3"
+    # "manishiitg/open-aditi-hi-v3"
     # "manishiitg/open-aditi-hi-v2-awq"
     # "manishiitg/open-aditi-hi-v1-awq"
     # "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ"
     # "teknium/OpenHermes-2.5-Mistral-7B"
-    # "manishiitg/open-aditi-hi-v2"
+    "manishiitg/open-aditi-hi-v2"
     # "manishiitg/open-aditi-hi-v1"
     "ai4bharat/Airavata"
     "sarvamai/OpenHathi-7B-Hi-v0.1-Base"
