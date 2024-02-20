2f0e1ac2c0a0d9b1f0817623573d402ec1acc7af
Manish Prakash <manish.prakash@citymall.live>
2024-02-20 08:10:17 +0530

    updates

---
+++ eval/load.py
@@ -0,0 +1,89 @@
+import argparse
+import random
+import torch
+import time
+from transformers import AutoTokenizer
+from bleurt import score
+import vllm
+
+def main(args):
+    random.seed(args.seed)
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.tokenizer_name_or_path if args.tokenizer_name_or_path else args.model_name_or_path)
+
+    if args.awq:
+        print("Loading model and tokenizer vllm awq...")
+        model = vllm.LLM(
+            model=args.model_name_or_path,
+            tokenizer=args.tokenizer_name_or_path if args.tokenizer_name_or_path else args.model_name_or_path,
+            tokenizer_mode="auto",
+            tensor_parallel_size=torch.cuda.device_count(),
+            quantization="AWQ",
+            max_model_len=4096,
+        )
+    else:
+        print("Loading model and tokenizer vllm...")
+        model = vllm.LLM(
+            model=args.model_name_or_path,
+            tokenizer=args.tokenizer_name_or_path if args.tokenizer_name_or_path else args.model_name_or_path,
+            tokenizer_mode="auto",
+            tensor_parallel_size=torch.cuda.device_count(),
+            max_model_len=4096,
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ntrain", type=int, default=0,
+                        help="number of examples to use for few-shot evaluation.")
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument(
+        "--lang", type=str, default="hindi", choices=["hindi", "english"]
+    )
+    parser.add_argument("--save_dir", type=str,
+                        default="/sky-notebook/eval-results/xlsum/llama-7B/")
+    parser.add_argument(
+        "--bleurt_model_name_or_path",
+        type=str,
+        default="./BLEURT-20",
+        help="bleurt model to load for evaluation.",
+    )
+    parser.add_argument(
+        "--model_name_or_path",
+        type=str,
+        default=None,
+        help="if specified, we will load the model to generate the predictions.",
+    )
+    parser.add_argument(
+        "--tokenizer_name_or_path",
+        type=str,
+        default=None,
+        help="if specified, we will load the tokenizer from here.",
+    )
+    parser.add_argument(
+        "--max_context_length", type=int, default=3750, help="maximum number of tokens in the context passage."
+    )
+    parser.add_argument(
+        "--n_instances",
+        type=int,
+        default=1000,
+        help="if specified, a maximum of n_instances will be used for the evaluation."
+    )
+    parser.add_argument(
+        "--use_chat_format",
+        action="store_true",
+        help="If given, we will use the chat format for the prompts.",
+    )
+    parser.add_argument(
+        "--chat_formatting_function",
+        type=str,
+        default="eval.templates.create_prompt_with_tulu_chat_format",
+        help="The function to use to create the chat format. This function will be dynamically imported. Please see examples in `eval/templates.py`.",
+    )
+    parser.add_argument(
+        "--awq",
+        action="store_true",
+        help="Load model as awq"
+    )
+    args = parser.parse_args()
+    main(args)
