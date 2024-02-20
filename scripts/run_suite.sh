2f3f4106fa45e49b9ffc9df8702f07808f5cf0d0
Manish Prakash <manish.prakash@citymall.live>
2024-02-20 14:40:45 +0530

    updates

--- scripts/run_suite.sh
+++ scripts/run_suite.sh
@@ -1,5 +1,5 @@
 # bash scripts/indic_eval/warmup.sh &
-bash scripts/indic_eval/lmjudge.sh
+# bash scripts/indic_eval/lmjudge.sh
 bash scripts/prepare_eval_data.sh
 bash scripts/indic_eval/indicxparaphrase.sh
 bash scripts/indic_eval/truthfulqa.sh
