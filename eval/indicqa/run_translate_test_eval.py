72324aaf04f9c778e1adf5e90eaa0e6f2a2473b6
Manish Prakash <manish.prakash@citymall.live>
2024-02-23 15:33:24 +0530

    updates

--- eval/indicqa/run_translate_test_eval.py
+++ eval/indicqa/run_translate_test_eval.py
@@ -141,6 +141,7 @@ def main(args):
     dataset = dataset.map(lambda x: {"context": x["context"].strip()})
     dataset = dataset.map(lambda x: {"question": x["question"].strip()})
     test_data = dataset["test"]
+    test_data = test_data.select(range(10))
     args.no_context = False
     k = args.ntrain
     sample_data = test_data.select(range(k*3))
@@ -203,8 +204,6 @@ def main(args):
             prompt = "\n\n".join([x["content"] for x in prompt])
 
         prompts.append(prompt)
-
-    prompts = prompts[:19]
 
     em_score = eval_hf_model(args, model, tokenizer,
                              prompts, test_data, args.eval_batch_size)
