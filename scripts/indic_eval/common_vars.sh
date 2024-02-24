959e667170226acef0bbe9e10e0554b65a11f01f
Manish Prakash <manish.prakash@citymall.live>
2024-02-24 21:42:42 +0530

    updates

--- scripts/indic_eval/common_vars.sh
+++ scripts/indic_eval/common_vars.sh
@@ -13,6 +13,6 @@ model_names=(
     "teknium/OpenHermes-2.5-Mistral-7B"
     "manishiitg/open-aditi-hi-v2"
     "manishiitg/open-aditi-hi-v1"   
-    # "sarvamai/OpenHathi-7B-Hi-v0.1-Base"
+    "sarvamai/OpenHathi-7B-Hi-v0.1-Base"
 )
 FOLDER_BASE=/sky-notebook/eval-results
