cb14d81ea0e604b810ebf45d335b4b064ed3ebe6
Manish Prakash <manish.prakash@citymall.live>
2024-02-26 15:31:35 +0530

    updates

--- scripts/indic_eval/common_vars.sh
+++ scripts/indic_eval/common_vars.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 model_names=(
+    "mistralai/Mixtral-8x7B-Instruct-v0.1"
     "google/gemma-7b-it"
     "Qwen/Qwen1.5-72B-Chat-AWQ"
     "Qwen/Qwen1.5-14B-Chat"
     "Qwen/Qwen1.5-4B-Chat"
     "Qwen/Qwen1.5-7B-Chat"
     "01-ai/Yi-34B-Chat"
-    # "mistralai/Mixtral-8x7B-Instruct-v0.1"
     "ai4bharat/Airavata"
     "manishiitg/open-aditi-hi-v4"
     "manishiitg/open-aditi-hi-v3"
