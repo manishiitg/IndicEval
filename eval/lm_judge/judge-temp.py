adec7b9f2979d236c56fb02fc9dd63154d0ff51a
Manish Prakash <manish.prakash@citymall.live>
2024-02-26 19:25:53 +0530

    updates

--- eval/lm_judge/judge-temp.py
+++ eval/lm_judge/judge-temp.py
@@ -124,8 +124,8 @@ def main(args):
     for row in ds:
         final_data.append(row)
 
-    # judge_model = "Qwen/Qwen1.5-7B-Chat"
-    judge_model = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+    judge_model = "Qwen/Qwen1.5-14B-Chat"
+    # judge_model = "mistralai/Mixtral-8x7B-Instruct-v0.1"
     tokenizer = AutoTokenizer.from_pretrained(judge_model)
 
     print("Loading model and tokenizer vllm awq...")
