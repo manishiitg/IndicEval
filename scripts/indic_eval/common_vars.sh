ad10e2e57dd7eaa12b22d658276f2265c6b4b515
Manish Prakash <manish.prakash@citymall.live>
2024-03-15 16:56:04 +0530

    eval for aditi v5

--- scripts/indic_eval/common_vars.sh
+++ scripts/indic_eval/common_vars.sh
@@ -5,7 +5,7 @@ model_names=(
     # "Qwen/Qwen1.5-72B-Chat-AWQ"
     # "Qwen/Qwen1.5-7B-Chat"
     # "01-ai/Yi-34B-Chat"
-    "mistralai/Mixtral-8x7B-Instruct-v0.1"
+    # "mistralai/Mixtral-8x7B-Instruct-v0.1"
     # "ai4bharat/Airavata"
     # "manishiitg/open-aditi-hi-v4"
     # "manishiitg/open-aditi-hi-v3"
@@ -14,6 +14,7 @@ model_names=(
     # "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ"
     # "teknium/OpenHermes-2.5-Mistral-7B"
     # "manishiitg/open-aditi-hi-v2"
+    "manishiitg/open-aditi-hi-v5"
     # "manishiitg/open-aditi-hi-v1"   
     # "sarvamai/OpenHathi-7B-Hi-v0.1-Base"
 )
