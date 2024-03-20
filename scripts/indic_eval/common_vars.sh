7ac6b2ba026b155025197f06e01337104a733278
Manish Prakash <manish.prakash@citymall.live>
2024-03-20 17:14:51 +0530

    updates

--- scripts/indic_eval/common_vars.sh
+++ scripts/indic_eval/common_vars.sh
@@ -2,10 +2,10 @@
 
 model_names=(
     "google/gemma-7b-it"
-    "Qwen/Qwen1.5-72B-Chat-AWQ"
-    "Qwen/Qwen1.5-7B-Chat"
-    "01-ai/Yi-34B-Chat"
-    "mistralai/Mixtral-8x7B-Instruct-v0.1"
+    # "Qwen/Qwen1.5-72B-Chat-AWQ"
+    # "Qwen/Qwen1.5-7B-Chat"
+    # "01-ai/Yi-34B-Chat"
+    # "mistralai/Mixtral-8x7B-Instruct-v0.1"
     "ai4bharat/Airavata"
     "manishiitg/open-aditi-hi-v4"
     "manishiitg/open-aditi-hi-v3"
@@ -14,5 +14,9 @@ model_names=(
     "manishiitg/open-aditi-hi-v2"
     "manishiitg/open-aditi-hi-v1"   
     "sarvamai/OpenHathi-7B-Hi-v0.1-Base"
+    "manishiitg/open-aditi-chat-hi-1.93-merged"
+    "manishiitg/open-aditi-chat-hi-1.91-merged"
+    # "manishiitg/open-aditi-chat-hi-1.92-merged"
+    "manishiitg/open-aditi-chat-hi-1.9-qwen-merged"
 )
 FOLDER_BASE=/sky-notebook/eval-results
