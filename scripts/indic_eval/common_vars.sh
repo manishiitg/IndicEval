b2680faabce3a7a8971a2d039b0cfeb55d1be97a
Manish Prakash <manish.prakash@citymall.live>
2024-04-08 07:01:20 +0530

    updates

--- scripts/indic_eval/common_vars.sh
+++ scripts/indic_eval/common_vars.sh
@@ -25,5 +25,6 @@ model_names=(
     "manishiitg/open-aditi-chat-hi-1.20-gemma-merged"
     "manishiitg/open-aditi-chat-hi-1.22-gemma-merged"
     "manishiitg/open-aditi-chat-hi-1.24-gemma-merged"
+    "manishiitg/open-aditi-chat-hi-1.25-gemma-merged"
 )
 FOLDER_BASE=/sky-notebook/eval-results
